8b2009d248e4ab13433b476b02adeb731ecd97b6
Neeraj Nittoori <neeraj.nittoori@coconstruct.com>
2022-01-17 13:46:13 -0500

    Commit #7

--- Playground1/Court4.cs
+++ Playground1/Court4.cs
@@ -12,6 +12,63 @@ namespace Playground1
             minTotalList.Add(new List<int>{1 , -1, -3});
 
             var minTot = MinimumTotal(minTotalList);
+            var twoSum = TwoSum(new int[]{1,3,1,4}, 6);
+            var nums1 = new int[] {1,2, 3, 0,0,0};
+            var nums2 = new int[]{2, 5, 6};
+            Merge(nums1, 3, nums2, 3);
+        }
+
+        public void Merge(int[] nums1, int m, int[] nums2, int n) {
+            var index = m + n - 1;
+            var i = m - 1;
+            var j = n - 1;
+            while(index >= 0 && (i >= 0 || j >= 0)) {
+                if(i < 0) {
+                    nums1[index] = nums2[j];
+                    j--;
+                }
+                else if(j < 0) {
+                    nums1[index] = nums1[i];
+                    i--;
+                }
+                else if(nums1[i] > nums2[j]) {
+                    nums1[index] = nums1[i];
+                    i--;
+                } else {
+                    nums1[index] = nums2[j];
+                    j--;
+                }
+
+                index--;
+            }
+        }
+
+        public int[] TwoSum(int[] nums, int target) {
+            var numDict = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++) {
+                numDict[nums[i]] = i;
+            }
+
+            for(int i = 0; i < nums.Length; i++) {
+                var requiredSum = target - nums[i];
+                if(numDict.ContainsKey(requiredSum) && numDict[requiredSum] != i) {
+                    return new int[]{i, numDict[requiredSum]};
+                }
+            }
+            return null;
+        }
+
+        public int MaxSubArray(int[] nums) {
+            if(nums.Length == 0) return 0;
+            int currentMaxVal =  nums[0];
+            int maxVal = nums[0];
+
+            for(int i = 1; i < nums.Length; i++) {
+                currentMaxVal = Math.Max(nums[i], currentMaxVal + nums[i]);
+                maxVal = Math.Max(maxVal, currentMaxVal);
+            }
+
+            return maxVal;  
         }
         
         Dictionary<int, List<int>> pascalTriangleDictionary = new Dictionary<int, List<int>>();
